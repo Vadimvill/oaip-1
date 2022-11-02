6e64bc8dad2d01a3a1b1e92fecf4913ae393428a
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-02 20:56:49 +0300

    Delete LR 5/lr 5 1 directory

--- LR 5/lr 5 1/lr 5 1/arry.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-int *arry_del_elements(int* size, int* arry);
--- LR 5/lr 5 1/lr 5 1/input.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<stdio.h>
-
-void input_arry_size(int* size);
-void input_arry(const int size, int* arry);
