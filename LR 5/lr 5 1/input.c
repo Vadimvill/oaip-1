bcc67b7b33c947284054896d82744c2828159380
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-02 20:59:19 +0300

    Add files via upload

---
+++ LR 5/lr 5 1/input.c
@@ -0,0 +1,24 @@
+#include "input.h"
+
+void input_arry_size(int* size)
+{
+	printf("Enter the arry size \n");
+	while (scanf_s("%d", size) == 0 || *size < 0 || getchar() != '\n')
+	{
+		printf("Wrong input.\n");
+		rewind(stdin);
+	}
+}
+
+void input_arry(const int size,int** arry)
+{
+	for (int  i = 0; i < size; i++)
+	{
+		while (scanf_s("%d", &(*arry)[i]) == 0 || getchar() != '\n')
+		{
+			printf("Wrong input.\n");
+			rewind(stdin);
+		}
+	}
+}
+
---
+++ LR 5/lr 5 1/output.h
@@ -0,0 +1,4 @@
+#pragma once
+#include<stdio.h>
+
+void output_arry(const int size, int** arry);
