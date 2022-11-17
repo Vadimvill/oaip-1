2569fc5a5c59e52f24a4c3e0f1ea8f538a4c6a9d
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-17 20:03:07 +0300

    Update input.h

--- LR 5/lr 5 3/lr 5 3/input.h
+++ LR 5/lr 5 3/lr 5 3/input.h
@@ -1,6 +1,6 @@
 #pragma once
 #include<stdio.h>
 
-void input_array_size(int* row,int* col);
+void input_array_size(int* array_size);
 void input_array(const int row, const int col, int*** array);
 
