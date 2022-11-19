a8be9ee7bb05a7a4807502151c5032cf7f1face5
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-19 20:50:48 +0300

    Update array.h

--- LR 6/LR 6 1/LR 6 1/array.h
+++ LR 6/LR 6 1/LR 6 1/array.h
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void array_memoey_allocate(const int array_size, int** array);
+void array_memory_allocate(const int array_size, int** array);
 int array_find_first_odd(int array_size, int** array);
 int array_find_first_negative(int array_size, int** array);
 void array_swap(int** array, int i, int j);
