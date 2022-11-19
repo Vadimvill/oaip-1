2a12dcb38457d51fdbf96bc899447ccfbfa92323
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-19 20:51:41 +0300

    Update array.h

--- LR 6/LR 6 2/LR 6 2/array.h
+++ LR 6/LR 6 2/LR 6 2/array.h
@@ -2,5 +2,5 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void array_memoey_allocate(const int row, const int col, int*** array);
+void array_memory_allocate(const int row, const int col, int*** array);
 int array_sum(int*** array, const int row, const int col);
