53b9fe9f42eafb4466bd3757f4db2cf6b7a3b578
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-19 20:46:49 +0300

    Update array.h

--- LR 5/lr 5 2/lr 5 2/array.h
+++ LR 5/lr 5 2/lr 5 2/array.h
@@ -4,7 +4,7 @@
 #include<time.h>
 #include"output.h"
 
-void array_memoey_allocate(const int row, const int col, int*** array);
+void array_memory_allocate(const int row, const int col, int*** array);
 void array_delete_max_elements(const int row, int*** array);
 void array_find_max_elements(const int row, const int col, int* max_elem, int*** array, const int i);
 void array_find_length_of_the_col(const int i, int* col, int*** array);
