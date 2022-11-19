b60f9baf1a526d2a69f93c36ae3030e559365e28
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-19 20:49:09 +0300

    Update array.h

--- LR 5/lr 5 3/lr 5 3/array.h
+++ LR 5/lr 5 3/lr 5 3/array.h
@@ -3,7 +3,7 @@
 #include<stdlib.h>
 
 
-void array_memoey_allocate(const int row, const int col, int*** array);
+void array_memory_allocate(const int row, const int col, int*** array);
 void array_del_elements(int* row, const int col, int*** array);
 void array_find_cols_with_2_zero(const int i, const int col, int*** array, int* zero_counter);
 void array_free(const int row, int** array);
