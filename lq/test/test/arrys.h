0e4d4a3d439ae5805f30af47b3690e83b7ecd05e
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-26 14:11:48 +0300

    Delete lq directory

--- lq/test/test/arrys.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-int** memory(int sizeofrows, int sizeofcols);
-void array_input_random(int** mas, int sizeofrows, int sizeofcols);
-void array_input_manually(int** mas, int sizeofrows, int sizeofcols);
