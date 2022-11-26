48309b362fb05bc7cbc139a58f048337c9c3c230
Matvey <112803286+GwinBest@users.noreply.github.com>
2022-11-26 10:58:48 +0300

    Delete lq/lr 5.2 directory

--- lq/lr 5.2/lr5-2/input.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-#include"arry.h"
-
-void input_arry_size(int* row);
-void input_arry(const int row, int*** arry);
-
--- lq/lr 5.2/lr5-2/output.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include<stdio.h>
-
-void output_arry(const int row, int*** arry);
