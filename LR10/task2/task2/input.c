1b7a241e672e0acfae9f2d14db95bfa82eed2ace
Matvey <thegwinbest@gmail.com>
2023-02-11 11:33:42 +0300

    refactoring

--- LR10/task2/task2/input.c
+++ LR10/task2/task2/input.c
@@ -2,14 +2,12 @@
 
 void file_init(FILE** file, int argc, char** argv)
 {
-	if (argc >= 3)
-	{
 		fopen_s(file, argv[1], "w+");
 
 		for (int i = 3; i < argc; i++)
 		{
 			fprintf(*file, "%s ", argv[i]);
 		}
-	}
+	
 }
 
--- LR10/task2/task2/main.c
+++ LR10/task2/task2/main.c
@@ -5,16 +5,15 @@
 
 int main(int argc, char** argv)
 {
-	char char_to_compare = argv[2][0];
 	FILE* kurinoe = NULL;
 
-	if (argc >= 2)
+	if (argc >= 3)
 	{
+		char char_to_compare = argv[2][0];
+
 		file_init(&kurinoe, argc, argv);
 		file_output(kurinoe);
 		char_compare(kurinoe, char_to_compare);
-		printf("\n");
-		
 		fclose(kurinoe);
 	}
 
