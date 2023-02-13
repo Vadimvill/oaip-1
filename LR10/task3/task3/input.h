13848abf24559bb48cd480fa5105be34197e31e6
Matvey <112803286+GwinBest@users.noreply.github.com>
2023-02-13 16:11:52 +0300

    Delete LR10 directory

--- LR10/task3/task3/input.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include<stdio.h>
-
-void file_init(FILE** file, int argc, char** argv);
--- LR10/task3/task3/reverse.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<io.h>
-
-void string_reverse(FILE* file, int start_point, int end_pioint);
