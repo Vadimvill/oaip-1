ef27e9603565b1e3125e44d3b3899dabf2af3d2e
Matvey <112803286+GwinBest@users.noreply.github.com>
2023-02-13 16:12:39 +0300

    Delete LR9 directory

--- LR9/task1/task1/input.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include"stdafx.h"
-
-void file_init(FILE** file, int argc, char** argv);
-int is_word(char* str);
--- LR9/task1/task1/output.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include"stdafx.h"
-
-void file_output(FILE* file);
