7572ca751dfca243756c5726576d17b32dab5170
Matvey <thegwinbest@gmail.com>
2023-02-18 22:36:30 +0300

    deleted lr9 directory

--- LR9/task2/task2/input.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<string.h>
-
-void file_init(FILE** file, int argc, char** argv);
--- LR9/task2/task2/output.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include<stdio.h>
-#include<stdlib.h>
-#include<io.h>
-
-void file_output(FILE* file);
